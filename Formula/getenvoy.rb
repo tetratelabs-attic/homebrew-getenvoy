fd56bb5a7a98c6bc7d1e7bbc3c300ff4210a526c
goreleaserbot <goreleaser@carlosbecker.com>
2019-08-30 08:06:38 +0100

    Brew formula update for getenvoy version v0.1.3

--- Formula/getenvoy.rb
+++ Formula/getenvoy.rb
@@ -2,16 +2,16 @@
 class Getenvoy < Formula
   desc "Distribute, deploy and operate a fleet of Envoys"
   homepage "https://github.com/tetratelabs/getenvoy"
-  version "0.1.2"
+  version "0.1.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/tetratelabs/getenvoy/releases/download/v0.1.2/getenvoy_0.1.2_Darwin_x86_64.tar.gz"
-    sha256 "69cfbc184f0f7ff7033eb5215319eae33c7b8424909690cad85ca96367f83906"
+    url "https://github.com/tetratelabs/getenvoy/releases/download/v0.1.3/getenvoy_0.1.3_Darwin_x86_64.tar.gz"
+    sha256 "7a382e841968dfe694a7b99d0613d7b38fcb0b319a83684e05fc0384ad70e1ff"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/tetratelabs/getenvoy/releases/download/v0.1.2/getenvoy_0.1.2_Linux_x86_64.tar.gz"
-      sha256 "77c5d49fb1adf314f0d06530e58541ac539f0e1255cf4b8d0740cf6736e47489"
+      url "https://github.com/tetratelabs/getenvoy/releases/download/v0.1.3/getenvoy_0.1.3_Linux_x86_64.tar.gz"
+      sha256 "f9ba123b9a3e511ae1f25d77575a9956ff5d91bb75b73a9e921e33afcbb8d8ec"
     end
   end
 
