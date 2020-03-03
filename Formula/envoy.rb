dfede5eeb144a703ac40fa86f38a20fa03dde892
Lizan Zhou <lizan@tetrate.io>
2020-03-03 10:40:31 -0800

    1.13.1 (#18)
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- Formula/envoy.rb
+++ Formula/envoy.rb
@@ -15,18 +15,18 @@
 class Envoy < Formula
     desc "Certified, compliant and conformant builds of Envoy proxy"
     homepage "https://getenvoy.io"
-    url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.12.0.p0.gab30caf-1p45.gf13ee1c-darwin-release-x86_64.tar.xz"
-    version "1.13.0"
-    sha256 "f569acc0cd71ef38f205be22b9f605bd5099c1b82af8c61a7227d1a3ea4c8a0c"
+    url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.13.1.p0.gb67c140-1p58.g95766ea-darwin-release-x86_64.tar.xz"
+    version "1.13.1"
+    sha256 "0c72bb39ad3b57e362a5a65a8973e0309b98b1868607a48a3e50785ad7e850cd"
 
     head do
-        url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.12.0.p0.gab30caf-1p45.gf13ee1c-darwin-release-x86_64.tar.xz"
-        sha256 "f569acc0cd71ef38f205be22b9f605bd5099c1b82af8c61a7227d1a3ea4c8a0c"
+        url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.13.1.p0.gb67c140-1p58.g95766ea-darwin-release-x86_64.tar.xz"
+        sha256 "0c72bb39ad3b57e362a5a65a8973e0309b98b1868607a48a3e50785ad7e850cd"
     end
 
     stable do
-        url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.12.0.p0.gab30caf-1p45.gf13ee1c-darwin-release-x86_64.tar.xz"
-        sha256 "f569acc0cd71ef38f205be22b9f605bd5099c1b82af8c61a7227d1a3ea4c8a0c"
+        url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.13.1.p0.gb67c140-1p58.g95766ea-darwin-release-x86_64.tar.xz"
+        sha256 "0c72bb39ad3b57e362a5a65a8973e0309b98b1868607a48a3e50785ad7e850cd"
     end
 
     bottle :unneeded
