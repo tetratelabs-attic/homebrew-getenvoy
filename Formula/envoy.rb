74ef0e8d7ea34c0ed338fcee98bb18b2db45fb56
Yaroslav Skopets <yaroslav@tetrate.io>
2020-09-29 22:40:51 +0200

    fix
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

--- Formula/envoy.rb
+++ Formula/envoy.rb
@@ -15,17 +15,17 @@
 class Envoy < Formula
     desc "Certified, compliant and conformant builds of Envoy proxy"
     homepage "https://getenvoy.io"
-    url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.15.1.p0.g670a4a6-1p69.ga5345f6-darwin-release-x86_64.tar.xz"
+    url "https://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.15.1.p0.g670a4a6-1p69.ga5345f6-darwin-release-x86_64.tar.xz"
     version "1.15.1"
     sha256 "4027cf80710e4a1ce5e8ba4462e511ec09368709c5517341fcdc1e5af2db288e"
 
     head do
-        url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.15.1.p0.g670a4a6-1p69.ga5345f6-darwin-release-x86_64.tar.xz"
+        url "https://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.15.1.p0.g670a4a6-1p69.ga5345f6-darwin-release-x86_64.tar.xz"
         sha256 "4027cf80710e4a1ce5e8ba4462e511ec09368709c5517341fcdc1e5af2db288e"
     end
 
     stable do
-        url "http://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.15.1.p0.g670a4a6-1p69.ga5345f6-darwin-release-x86_64.tar.xz"
+        url "https://dl.bintray.com/tetrate/getenvoy/getenvoy-envoy-1.15.1.p0.g670a4a6-1p69.ga5345f6-darwin-release-x86_64.tar.xz"
         sha256 "4027cf80710e4a1ce5e8ba4462e511ec09368709c5517341fcdc1e5af2db288e"
     end
 
